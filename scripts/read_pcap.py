76b43daa4c78c077f3e0c9ed8070b7f9b03183d6
Antoine Albertelli <antoine@antoinealb.net>
2019-01-07 17:31:35 +0100

    Filter read packets

--- scripts/read_pcap.py
+++ scripts/read_pcap.py
@@ -12,6 +12,7 @@ from r2p2 import *
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("input", type=argparse.FileType('rb'))
+    parser.add_argument("--all", help="Show all packets, not only those related to Raft", action="store_true")
 
     return parser.parse_args()
 
@@ -22,6 +23,14 @@ def main():
     for p in packets:
         if not "R2P2" in p:
             continue
+
+        if not args.all:
+            if not "Raft" in p:
+                continue
+
+            if p["Raft"].type not in (0,1):
+                continue
+
         p.show()
 
 
