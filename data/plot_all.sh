9c1bbf9ff887e97d339bc70762cc7ae2a1ad0250
Antoine Albertelli <antoine@antoinealb.net>
2019-01-22 08:21:20 +0100

    Make plots more beautiful

--- data/plot_all.sh
+++ data/plot_all.sh
@@ -7,6 +7,13 @@ cd $(dirname $0)
 mkdir -p ../thesis/figures/plots/
 
 ../scripts/plot/plot_lancet.py \
+    --data single_node_10_us_synthetic.txt --legend "No replication" \
     --data replicated_2_replicas_10_us_synthetic.txt --legend "2 replicas" \
-    --data single_node_dpdk_10us_synthetic.txt --legend "No replication" \
+    --data replicated_2_replicas_10_us_synthetic_leader_only.txt --legend "2 replicas, workload on leader" \
+    --ymax 1000 \
     --output ../thesis/figures/plots/latency_10us_synthetic.pdf
+
+../scripts/plot/plot_lancet.py \
+    --data replicated_2_replicas_0_us_synthetic.txt --legend "2 replicas" \
+    --data single_node_0_us_synthetic.txt --legend "No replication" \
+    --output ../thesis/figures/plots/latency_0us_synthetic.pdf
--- scripts/plot/plot_lancet.py
+++ scripts/plot/plot_lancet.py
@@ -7,12 +7,14 @@ import argparse
 import lancet
 import plottools
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter, FuncFormatter
 
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--data", "-d",  type=argparse.FileType(), help='Results from Lancets stdout', action='append')
     parser.add_argument("--legend", "-l", action='append', help='Legend, one per data file')
     parser.add_argument("--output", "-o", help='Name of the output figure filename')
+    parser.add_argument("--ymax", "-y", help='Max latency on Y coordinate.', default=200, type=int)
 
     return parser.parse_args()
 
@@ -20,7 +22,7 @@ def plot_file(f, markers):
     data = f.read()
     data = lancet.parse_results(data)
 
-    x = [p.rate for p in data]
+    x = [p.rate // 1000 for p in data]
     y = [p.latency_percentiles[99] for p in data]
 
     plt.plot(x, y, markers)
@@ -33,13 +35,22 @@ def main():
     for f, marker in zip(args.data, markers):
         plot_file(f, marker)
 
-    plt.xlabel('Load (Request per second)')
-    plt.ylabel(r'Latency ($\mu$s)')
-    plt.ylim(0, 200)
+    plt.xlabel('Throughput (1000 of requests per second)')
+    plt.ylabel(r'Latency (99th percentile)')
+    plt.ylim(0, args.ymax)
     plt.grid()
 
+    # Set units on Y axis
+    plt.gca().yaxis.set_major_formatter(FormatStrFormatter('%d $\mu$s'))
+
+    # Set thousands separator on X axis
+    fmt = FuncFormatter(lambda x, loc: "{:,}".format(int(x)))
+    plt.gca().xaxis.set_major_formatter(fmt)
+
+    plt.tight_layout()
+
     if args.legend:
-        plt.legend(args.legend)
+        plt.legend(args.legend, loc='upper left')
 
     if args.output:
         plt.savefig(args.output)
