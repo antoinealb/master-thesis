3863ff68c7874c1dfa611109b0a1705d7b72ab41
Antoine Albertelli <antoine@antoinealb.net>
2019-01-31 17:22:00 +0100

    Add all the various benchmarks

--- data/plot_all.sh
+++ data/plot_all.sh
@@ -7,21 +7,29 @@ cd $(dirname $0)
 mkdir -p ../thesis/figures/plots/
 
 ../scripts/plot/plot_lancet.py \
-    --data single_node_10_us_synthetic.txt --legend "No replication" \
-    --data replicated_2_replicas_10_us_synthetic.txt --legend "2 replicas" \
-    --data replicated_2_replicas_10_us_synthetic_leader_only.txt --legend "2 replicas, workload on leader" \
-    --ymax 600 \
+    --data single_node_10_us_synthetic.txt --legend "Unreplicated" -m "x-" \
+    --data replicated_3_replicas_10_us_synthetic.txt --legend "3 replicas" -m "o-" \
+    --data replicated_3_replicas_10_us_synthetic_singlethread.txt --legend "3 replicas (no worker thread)" -m "o--" \
+    --ymax 400 \
     --output ../thesis/figures/plots/latency_10us_synthetic.pdf
 
 ../scripts/plot/plot_lancet.py \
-    --data replicated_2_replicas_0_us_synthetic.txt --legend "2 replicas" \
-    --data single_node_0_us_synthetic.txt --legend "No replication" \
+    --data single_node_1_us_synthetic.txt --legend "Unreplicated" -m "x-" \
+    --data replicated_3_replicas_1_us_synthetic.txt --legend "3 replicas" -m "o-" \
+    --data replicated_3_replicas_1_us_synthetic_singlethread.txt --legend "3 replicas (no worker thread)" -m "o--" \
     --ymax 80 \
-    --output ../thesis/figures/plots/latency_0us_synthetic.pdf
+    --output ../thesis/figures/plots/latency_1us_synthetic.pdf
 
 ../scripts/plot/plot_lancet.py \
-    --data single_node_1_us_synthetic.txt --legend "Unreplicated" \
-    --data replicated_3_replicas_1_us_synthetic.txt --legend "3 replicas" \
-    --data replicated_5_replicas_1_us_synthetic.txt --legend "5 replicas" \
+    --data single_node_1_us_synthetic.txt --legend "Unreplicated" -m "x-" \
+    --data replicated_3_replicas_1_us_synthetic.txt --legend "3 replicas" -m "o-" \
+    --data replicated_5_replicas_1_us_synthetic.txt --legend "5 replicas" -m "v-" \
     --ymax 80 \
-    --output ../thesis/figures/plots/cluster_size.pdf
+    --output ../thesis/figures/plots/cluster_size_1us.pdf
+
+../scripts/plot/plot_lancet.py \
+    --data single_node_10_us_synthetic.txt --legend "Unreplicated" -m "x-" \
+    --data replicated_3_replicas_10_us_synthetic.txt --legend "3 replicas" -m "o-" \
+    --data replicated_5_replicas_10_us_synthetic.txt --legend "5 replicas" -m "v-" \
+    --ymax 400 \
+    --output ../thesis/figures/plots/cluster_size_10us.pdf
